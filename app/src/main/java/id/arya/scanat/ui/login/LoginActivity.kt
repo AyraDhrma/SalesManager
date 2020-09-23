e75f712c7fdbd464d7f68e0c037fa50f0f8f538e
aryadharma <aryadharma5007@gmail.com>
2020-09-23 22:33:48 +0700

    init arya

---
+++ app/src/main/java/id/arya/scanat/ui/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package id.arya.scanat.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.arya.scanat.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
